d69a0b930e164862c81db9d01f630d4ab3327333
cla-gambino <154328607+cla-gambino@users.noreply.github.com>
2024-04-05 10:34:56 +0200

    convertStringToDate method

--- src/main/java/develhope/esercizio72ControllerconGETcompleta/LocationWeatherController.java
+++ src/main/java/develhope/esercizio72ControllerconGETcompleta/LocationWeatherController.java
@@ -2,28 +2,36 @@ package develhope.esercizio72ControllerconGETcompleta;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeParseException;
+
 @RestController
 @RequestMapping("/v2")
 public class LocationWeatherController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/ciao/{provincia}")
-
-
-    public String ciao(
-
+    public User ciao(
             @PathVariable(value = "provincia", required = true) String provincia,
-            @RequestParam(value = "nome", required = false) String nome) {
+            @RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "dataDiNascita", required = false) String dataDiNascita) {
 
 
         if (nome == null) {
-            return "hai dimenticato il nome";
+            return new User();
         } else {
             String saluto = "Ciao " + nome + ", com'è il tempo in " + provincia + "?";
-            User user = new User(nome, provincia, saluto);
-            return user.toString();
-
-
+            User user = new User(nome, provincia, saluto, convertStringToDate(dataDiNascita));
+            return user;
         }
     }
 
+    private OffsetDateTime convertStringToDate(String dataDiNascita) {
+        try {
+            OffsetDateTime dDN = OffsetDateTime.parse(dataDiNascita);
+            return dDN;
+        } catch (DateTimeParseException exception) {
+            System.out.println("inserisci correttamente la data di nascita");
+            return null;
+        }
+    }
 }
--- src/main/java/develhope/esercizio72ControllerconGETcompleta/User.java
+++ src/main/java/develhope/esercizio72ControllerconGETcompleta/User.java
@@ -1,16 +1,24 @@
 package develhope.esercizio72ControllerconGETcompleta;
 
+import java.time.OffsetDateTime;
+
 public class User {
 
-    public User(String nome, String provincia, String saluto) {
+    public User(String nome, String provincia, String saluto, OffsetDateTime dataDiNascita) {
         this.nome = nome;
         this.provincia = provincia;
         this.saluto = saluto;
+        this.dataDiNascita = dataDiNascita;
+    }
+
+    public User() {
+
     }
 
     public String nome;
     public String provincia;
     public String saluto;
+    public OffsetDateTime dataDiNascita;
 
 
     @Override
@@ -19,6 +27,7 @@ public class User {
                 "nome='" + nome + '\'' +
                 ", provincia='" + provincia + '\'' +
                 ", saluto='" + saluto + '\'' +
+                ", dataDiNascita=" + dataDiNascita +
                 '}';
     }
 }
